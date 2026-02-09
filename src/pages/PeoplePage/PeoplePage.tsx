2c9bcad61a1065f4c2a491a3f60e63825f4016ad
Vadym Vasyliev <protocol222333@gmail.com>
2026-02-09 14:30:50 +0200

    add task solution

---
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+
+import { wait } from '../../helpers/helpers';
+import { Person } from '../../types';
+import { PeopleTable } from '../../components/PeopleTable';
+import { Loader } from '../../components/Loader';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const loadData = async () => {
+      setIsLoading(true);
+      setIsError(false);
+
+      try {
+        const [response] = await Promise.all([
+          fetch('api/people.json'),
+          wait(500),
+        ]);
+
+        if (!response) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+
+        setPeople(data);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadData();
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && <Loader />}
+
+          {!isLoading && isError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!isLoading && !isError && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!isLoading && !isError && people.length > 0 && (
+            <PeopleTable people={people} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
